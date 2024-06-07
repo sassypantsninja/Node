5211c44ac6da583bcd4812cd49fe9167c5705536
sassypantsninja <117579537+sassypantsninja@users.noreply.github.com>
2024-06-07 18:43:34 +0530

    SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public data;
+
+    function setData(uint256 _data) public {
+        data = _data;
+    }
+
+    function getData() public view returns (uint256) {
+        return data;
+    }
+}
